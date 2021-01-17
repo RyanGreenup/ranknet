02b058a3c002e10301de5fe7a6ecda1e059bd85a
Ryan Greenup <exogenesis@protonmail.com>
2021-01-18 09:35:13 +1100

    Fixed Misclassification Rate which was backwards

--- scripts/neural_network/Moons/tools.py
+++ scripts/neural_network/Moons/tools.py
@@ -15,7 +15,7 @@ def misclassification_rate(target: numpy.ndarray, output: numpy.ndarray):
         print("Input values must both be Numpy Arrays filled with integers!")
 
     if numpy.shape(target) == numpy.shape(output):
-        misclassification_rate = numpy.average(target == output)
+        misclassification_rate = numpy.average(target != output)
     else:
         raise IndexError('Target and Output Arrays must be the same shape')
 
