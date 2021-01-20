d2e6c12d582fb1799b1a5f6997972dd76605d3a6
Ryan Greenup <exogenesis@protonmail.com>
2021-01-20 12:54:24 +1100

    currently restructuring wine data

--- scripts/neural_network/wine_neural_network.py
+++ scripts/neural_network/wine_neural_network.py
@@ -3,55 +3,60 @@ import os, sys
 os.chdir(os.path.dirname(sys.argv[0]))
 # os.chdir("/home/ryan/Studies/2020ResearchTraining/ranknet/scripts/neural_network/")
 
-#    _  _     ___                            _   
-#  _| || |_  |_ _|_ __ ___  _ __   ___  _ __| |_ 
+# * Import Packages
+#    _  _     ___                            _
+#  _| || |_  |_ _|_ __ ___  _ __   ___  _ __| |_
 # |_  ..  _|  | || '_ ` _ \| '_ \ / _ \| '__| __|
-# |_      _|  | || | | | | | |_) | (_) | |  | |_ 
+# |_      _|  | || | | | | | |_) | (_) | |  | |_
 #   |_||_|   |___|_| |_| |_| .__/ \___/|_|   \__|
-#                          |_|                   
-#  ____            _                         
-# |  _ \ __ _  ___| | ____ _  __ _  ___  ___ 
+#                          |_|
+#  ____            _
+# |  _ \ __ _  ___| | ____ _  __ _  ___  ___
 # | |_) / _` |/ __| |/ / _` |/ _` |/ _ \/ __|
 # |  __/ (_| | (__|   < (_| | (_| |  __/\__ \
 # |_|   \__,_|\___|_|\_\__,_|\__, |\___||___/
 #                            |___/
 
-# Typical stuff
+# ** Typical stuff
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-# OS Stuff
-import sys, os
-
-# Torch Stuff
+# ** Torch Stuff
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# * Main
+############################################################
 #  __  __       _
 # |  \/  | __ _(_)_ __
 # | |\/| |/ _` | | '_ \
 # | |  | | (_| | | | | |
 # |_|  |_|\__,_|_|_| |_|
+#
+############################################################
 
 
 def main():
+    # Load Data ................................................
     X, X_test, y, y_test = load_data()
 
-    # Assign the model object
-    main.net = Network()
+    # Assign the Model Object ....................................
+    main.net = NeuralNetwork()
     print('The Neural Network is described as:\n')
     print(main.net)
 
-    # Define the Loss Function
-    loss_fn = nn.L1Loss()
-    eta = 1/1000
 
-    # Define the Optimizer
+    # Choose a Loss Function  ....................................
+    loss_fn = torch.nn.L1Loss()
+    eta = 1/9000
+
+    # Choose an Optimizer
     optimizer = torch.optim.RMSprop(main.net.parameters(), lr = eta)
 
+    # Train the Model ............................................
     main.net.train_model(main.net, optimizer, loss_fn, X, y)
 
     ## Print the Model Output
@@ -60,23 +65,18 @@ def main():
     print(out)
 
     # Measure the misclassification rate
-    print('\n----------------------------\n')
-    print("The Training Missclassification rate is:\n")
-    misclassification_rate(X, y)
-    print('\n----------------------------\n')
-    print("The Testing Missclassification rate is:\n")
-    misclassification_rate(X_test, y_test)
+    m = misclassification_rate(X, y, X_test, y_test)
+    m.measure_train()
+    m.report()
 
     # Print the losses
-    # TODO should I make train_model a class so that I can use self, that way if I change the function name...
-    # TODO should the train_model function be a part of the Neural Network class? That might be cleaner?
     plt.plot(main.net.losses)
     plt.show()
 
 
 
-#  _                    _   ____        _        
-# | |    ___   __ _  __| | |  _ \  __ _| |_ __ _ 
+#  _                    _   ____        _
+# | |    ___   __ _  __| | |  _ \  __ _| |_ __ _
 # | |   / _ \ / _` |/ _` | | | | |/ _` | __/ _` |
 # | |__| (_) | (_| | (_| | | |_| | (_| | || (_| |
 # |_____\___/ \__,_|\__,_| |____/ \__,_|\__\__,_|
@@ -101,38 +101,31 @@ def load_data(datafile='./DataSets/winequality-red.csv'):
     return X, X_test, y, y_test
 
 
-# |  \/  (_)___  ___| | __ _ ___ ___(_)/ _(_) ___ __ _| |_(_) ___  _ __  
-# | |\/| | / __|/ __| |/ _` / __/ __| | |_| |/ __/ _` | __| |/ _ \| '_ \ 
+# |  \/  (_)___  ___| | __ _ ___ ___(_)/ _(_) ___ __ _| |_(_) ___  _ __
+# | |\/| | / __|/ __| |/ _` / __/ __| | |_| |/ __/ _` | __| |/ _ \| '_ \
 # | |  | | \__ \ (__| | (_| \__ \__ \ |  _| | (_| (_| | |_| | (_) | | | |
 # |_|  |_|_|___/\___|_|\__,_|___/___/_|_| |_|\___\__,_|\__|_|\___/|_| |_|
-                                                                       
-#  ____       _       
-# |  _ \ __ _| |_ ___ 
+
+#  ____       _
+# |  _ \ __ _| |_ ___
 # | |_) / _` | __/ _ \
 # |  _ < (_| | ||  __/
 # |_| \_\__,_|\__\___|
-                    
-
-def misclassification_rate(X, y):
-    yhat = main.net(X)
-    yhat = yhat.detach().numpy().reshape(-1) > 0.5
-
-    y=np.array(y)
-
-    print(np.average(y != yhat))
 
 
 
-#  _   _                      _   _   _      _                      _    
+
+
+#  _   _                      _   _   _      _                      _
 # | \ | | ___ _   _ _ __ __ _| | | \ | | ___| |___      _____  _ __| | __
 # |  \| |/ _ \ | | | '__/ _` | | |  \| |/ _ \ __\ \ /\ / / _ \| '__| |/ /
-# | |\  |  __/ |_| | | | (_| | | | |\  |  __/ |_ \ V  V / (_) | |  |   < 
+# | |\  |  __/ |_| | | | (_| | | | |\  |  __/ |_ \ V  V / (_) | |  |   <
 # |_| \_|\___|\__,_|_|  \__,_|_| |_| \_|\___|\__| \_/\_/ \___/|_|  |_|\_\
-                                                                       
+
 
 # Define the Class for Torch
 
-class Network(nn.Module):
+class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         self.losses = []
@@ -149,22 +142,19 @@ class Network(nn.Module):
 
     def forward(self, x):
         # Take input
-        x = self.hidden_1(x)  # Linear Combination of input-> hidden
-        x = self.sigmoid(x) # Activation Function
-        x = self.hidden_mid(x)  # Linear Combination of input-> hidden
-        x = self.sigmoid(x) # Activation Function
-        x = self.hidden_mid(x)  # Linear Combination of input-> hidden
-        x = self.sigmoid(x) # Activation Function
-        x = self.output(x)  # Linear Combination of hidden -> output
-        x = self.sigmoid(x) # Activation Function
+        x = self.hidden_1(x)   # Linear Combination of input-> hidden
+        x = self.sigmoid(x)    # Activation Function
+        x = self.hidden_mid(x) # Linear Combination of input-> hidden
+        x = self.sigmoid(x)    # Activation Function
+        x = self.output(x)     # Linear Combination of hidden -> output
+        x = self.sigmoid(x)    # Activation Function
         x = torch.flatten(x, start_dim=0, end_dim=-1)
 
         return x
 
     def train_model(self, model, optimizer, loss_fn, X, y):
         self.losses = []
-        # for t in range(4000):  # loop over the dataset multiple times
-        for t in range(5000):  # loop over the dataset multiple times
+        for t in range(int(8e1)):  # loop over the dataset multiple times
             # Forward Pass; Calculate the Prediction
             y_pred = model(X)
 
@@ -183,14 +173,35 @@ class Network(nn.Module):
             # update the Weights
             optimizer.step()
 
+class misclassification_rate:
+    """
+    This contains the functions to measure and report misclassification rates
+    """
+    def __init__(self, X, y, X_test, y_test):
+        self.X = X
+        self.y = y
+        self.X_test = X_test
+        self.y_test = y_test
 
-# loss_fn = nn.BCEWithLogitsLoss()
+    def measure_train(self):
+        yhat = main.net(self.X)
+        yhat = yhat.detach().numpy().reshape(-1) > 0.5
+        y=np.array(self.y)
+        print(np.average(y != yhat))
 
-# |_   _| __ __ _(_)_ __   | |_| |__   ___  |  \/  | ___   __| | ___| |
-#   | || '__/ _` | | '_ \  | __| '_ \ / _ \ | |\/| |/ _ \ / _` |/ _ \ |
-#   | || | | (_| | | | | | | |_| | | |  __/ | |  | | (_) | (_| |  __/ |
-#   |_||_|  \__,_|_|_| |_|  \__|_| |_|\___| |_|  |_|\___/ \__,_|\___|_|
+    def measure_test(self):
+        yhat = main.net(self.X_test)
+        yhat = yhat.detach().numpy().reshape(-1) > 0.5
+        y=np.array(self.y_test)
+        print(np.average(y != yhat))
 
+    def report(self):
+        print('\n----------------------------\n')
+        print("The Training Missclassification rate is:\n")
+        self.measure_train()
+        print('\n----------------------------\n')
+        print("The Testing Missclassification rate is:\n")
+        self.measure_test()
 
 
 main()
