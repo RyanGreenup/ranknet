67495d96f4c1a99bafd1d52d989e19650b0c7219
Ryan Greenup <exogenesis@protonmail.com>
2021-02-13 15:26:12 +1100

    Now Happy with sorting algorithm

--- scripts/neural_network/my_quicksort.py
+++ scripts/neural_network/my_quicksort.py
@@ -1,55 +1,43 @@
-import random
+# https://www.geeksforgeeks.org/python-program-for-quicksort/
+DEBUG = False
 
-values = [2, 1, 7, 8, 5, 0, 4, 6, 3]
-# values = random.sample(range(9), 9)
-N = len(values)
+def split(values, left, high): 
+	# Define the leftmost value
+	l = (left-1)
+	# Set the right value as the pivot
+	pivot = values[high]	 
 
+	for q in range(left, high): 
+		# Only move smaller values left
+		if values[q] <= pivot: 
+			# +1 next left element
+			l = l+1
+			# Swap the current element onto the left
+			values[l], values[q] = values[q], values[l] 
 
-
-def partition(values, left, right):
-    #region
-    '''
-    Take a list of values and rearrange so that the first term is in the centre, terms less than the first entry
-    appear to the left and all terms greater appear on the right, e.g.
-
-        [5, 3, 1, 6, 8, 9] => 5: [3, 1, 5, 6, 8, 9]
-    
-    The order of the terms around the "pivot" don't matter, so the following would also be acceptable:
-
-        [5, 3, 1, 6, 8, 9] => 5: [1, 3, 5, 8, 9, 6]
-
-    Parameters:
-        values (list): A list of values that need to be sorted
-    Output: 
-
-        pi (int): An integer describing the index value of the pivot point,
-                  i.e. the location that the first element was moved to.
-    '''
-    #endregion
-    l = left      # Current left Most Value
-    p = values[0] # Use the first value as a pivot point
-
-    for i in range(left, right):  # TODO and i not 0 # Skip the first value
-        if values[i] <= p:
-            print("Swapping index ", i, " and ", l)
-            swap(values, i, l)   # If the value is less than
-            l+=1                    # the pivot, swap it with a left term
-    
-    swap(values, 0, l-1) # Swap the pivot out of the 1st index into correct spot
-    return l    # Return the index of the pivot which should be straight after the left most value
-
-def swap(values, i, j):
-    ival = values[i]
-    jval = values[j]
-    values[i] = jval
-    values[j] = ival
+    # Swap the pivot value into the left position from the right
+	values[l+1], values[high] = values[high], values[l+1] 
+	return (l+1) 
 
 
+def quicksort(values, left, right): 
+	if len(values) == 1: 
+		return values 
+	if right > left:
+		# pi is the index of where the pivot was moved to
+		# It's position is now correct
+		pi = split(values, left, right) 
 
-print(values)
-pi = partition(values, 0, N)
-print(pi)
-print(values)
+		# Do this again for the left and right parts
+		quicksort(values, left, pi-1) 
+		quicksort(values, pi+1, right) 
 
 
-
+if DEBUG:
+	for i in range(3):
+		import random
+		values = random.sample(range(9), 7)
+		n = len(values) 
+		print(values)
+		quicksort(values, 0, n-1) 
+		print("==>", values) 
