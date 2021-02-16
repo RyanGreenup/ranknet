cf9ab26ae9282f77ffd183bb0d71324280dd5323
Ryan Greenup <exogenesis@protonmail.com>
2021-02-16 14:50:01 +1100

    Generate data to use for classification

--- scripts/Blobs_Neural_Network.py
+++ scripts/Blobs_Neural_Network.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python
 
 # Import Packages
-from sklearn import datasets
 import numpy as np
-from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import torch
 import sys
 from torch import nn
 import random
 from ranknet.test_cuda import test_cuda
+from ranknet.make_data import make_data
 
 # Set Seeds
-torch.manual_seed(1)  
+torch.manual_seed(1)
 np.random.seed(1)
 
 # Set Torch Parameters
@@ -26,4 +25,9 @@ DEBUG = True
 # Main Function
 
 def main():
-    print("Hello World!")
+    X_train, X_test, y_train, y_test = make_data(
+        n=100, create_plot=True, dtype=dtype, dev=dev)
+
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/ranknet/make_data.py
@@ -0,0 +1,30 @@
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+
+
+def make_data(create_plot=False, n=1000, dtype=torch.float, dev="cpu"):
+    X, y = datasets.make_blobs(n, 2, 2, random_state=7) 
+    # temp_X, y = datasets.make_moons(n_samples=n, noise=0.1, random_state=0) # Moons Data for later
+
+    # Reshape the data to be consistent
+    y = np.reshape(y, (len(y), 1))  # Make y vertical n x 1 matrix.
+
+    # -- Split data into Training and Test Sets --------------------
+    data = train_test_split(X, y, test_size=0.4)
+
+    if(create_plot):
+        # Create the Scatter Plot
+        plt.scatter(X[:, 0], X[:, 1], c=y)
+        plt.title("Sample Data")
+        plt.show()
+
+    # Make sure we're working with tensors not mere numpy arrays
+    torch_data = [None]*len(data)
+    for i in range(len(data)):
+        torch_data[i] = torch.tensor(data[i], dtype=dtype, requires_grad=False)
+
+    return torch_data
